f97e5113a99db5bfefcdbcf09b82ad35590e09be
Donaldson Tan <donaldsontan@alma.shukra-networks.com>
2024-08-15 01:48:06 +0800

    added prefix _ to internal functions _addHandlers(), _validUnitCode(), _getName()

--- bot.py
+++ bot.py
@@ -8,19 +8,19 @@ import logging
 def setupBot(token:str, admins:List[str], logger:logging.Logger, dbname:str) -> AsyncTeleBot:
     bot = AsyncTeleBot(token)
     db = shelve.open(dbname)
-    addHandlers(bot, admins, db, logger)
+    _addHandlers(bot, admins, db, logger)
     return bot
 
-def getName(message: telebot.types.Message) -> str:
+def _getName(message: telebot.types.Message) -> str:
     name = message.from_user.username
     if name == "":
         name = message.from_user.full_name
     return name
 
-def validUnitCode(unitcode:str) -> bool:
+def _validUnitCode(unitcode:str) -> bool:
     return len(unitcode) == 6 and unitcode[:3].islapha() and unitcode[3:].isnumeric()
 
-def addHandlers(bot: AsyncTeleBot, admins: List[str], db: shelve.Shelf, logger:logging.Logger) -> None:
+def _addHandlers(bot: AsyncTeleBot, admins: List[str], db: shelve.Shelf, logger:logging.Logger) -> None:
 
     @bot.message_handler(commands=['start','welcome'])
     async def welcome(message:telebot.types.Message) -> telebot.types.Message:
@@ -38,7 +38,7 @@ def addHandlers(bot: AsyncTeleBot, admins: List[str], db: shelve.Shelf, logger:l
             unitcode = tokens[1].upper()
             unitname = " ".join(tokens[3:])
             link = tokens[2]
-            if validUnitCode(unitcode):
+            if _validUnitCode(unitcode):
                 if link.startswith("https://t.me/"):
                     if len(unitname) > 0:
                         db[unitcode] = (unitname, link)
@@ -56,8 +56,8 @@ def addHandlers(bot: AsyncTeleBot, admins: List[str], db: shelve.Shelf, logger:l
                 logger.error(f"{message.from_user.username} attempted to add malformed unit code {unitcode}")
                 msg = "Malformed unit code {unitcode}"
         else:
-            logger.error(f"Unauthorised user f{getName(message)} attempted to perform {message.text}")
-            msg = f"{getName(message)} is Unauthorised user"
+            logger.error(f"Unauthorised user f{_getName(message)} attempted to perform {message.text}")
+            msg = f"{_getName(message)} is Unauthorised user"
         if not success:
             await bot.reply_to(message, f"Fail because {msg}")
    
@@ -71,7 +71,7 @@ def addHandlers(bot: AsyncTeleBot, admins: List[str], db: shelve.Shelf, logger:l
         if message.from_user.username in admins:
             tokens = message.text.split()
             unitcode = tokens[1].upper()
-            if validUnitCode(unitcode):
+            if _validUnitCode(unitcode):
                 if unitcode in db:
                     success = True
                     del db[unitcode]
@@ -82,7 +82,7 @@ def addHandlers(bot: AsyncTeleBot, admins: List[str], db: shelve.Shelf, logger:l
             else:
                 msg = "Malformed unit code"
         else:
-            msg = f"{getName(message)} is Unauthorised user"
+            msg = f"{_getName(message)} is Unauthorised user"
         if not success:
             await bot.reply(message, f"Fail because {msg}")
     
@@ -112,7 +112,7 @@ def addHandlers(bot: AsyncTeleBot, admins: List[str], db: shelve.Shelf, logger:l
                     prev = u
                 if len(units) > 0:
                     bot.reply_to(message, "\n".join(units))
-        elif validUnitCode(unitcode):
+        elif _validUnitCode(unitcode):
             if unitcode in db:
                 success = True
                 unitname, link = db[unitcode]
