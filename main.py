e7d1be3c5ae23d178a1c34864894f6637326ea0c
Donaldson Tan <donaldsontan@alma.shukra-networks.com>
2024-08-14 14:23:13 +0800

    initial commit of main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+import logging 
+import config
+import bot
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger(__name__)
+    try:
+        token, admins = config.readConfig()
+    except config.ConfigException as e:
+        logger.error(e)
+        logger.info("suss-telegram-groups bot terminates")
+    else:
+        b = bot.Bot(token, admins, logger)
+        b.infinity_polling()
