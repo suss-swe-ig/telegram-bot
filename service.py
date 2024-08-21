2e2264c4eafb897f0ae89298418495c4486c3005
Donaldson Tan <geodome@gmail.com>
2024-08-21 11:59:01 +0800

    setup service layer

--- service.py
+++ service.py
@@ -10,13 +10,26 @@ from telebot.async_telebot import AsyncTeleBot
 from singleton import Singleton
 from businesslogic import User, Admin, NonAdminUserException
 
+_SERVICE = None
+
+def setup(token:str, logger:logging.Logger):
+    global _SERVICE
+    _SERVICE = Service(token, logger)
+
+class ServiceNotReadyException(Exception):
+    pass
+
+def run():
+    global _SERVICE
+    if _SERVICE is None:
+        raise ServiceNotReadyException()
+    _SERVICE.run()
+    
 class Service(Singleton):
 
-    def __init__(self, token:str, admins:List[str], logger:logging.Logger, database:shelve.Shelf) -> None:
+    def __init__(self, token:str, logger:logging.Logger) -> None:
         self._token = token
-        self._admins = admins 
         self._logger = logger
-        self._db = database
         self._telebot = AsyncTeleBot(token)
         self._addHandlers()
 
@@ -30,7 +43,9 @@ class Service(Singleton):
             /start      Says hi to user
             /welcome    Says hi to user
             """
-            await User(message.from_user.username, message.from_user.full_name, self._admins, self._db, self._logger, self._telebot).welcome(message)
+            replies = User(message.from_user.username, message.from_user.full_name, self._logger).welcome(message)
+            for reply in replies:
+                await self._telebot.reply_to(message, reply)
 
         @self._telebot.message_handler(commands=['get'])
         async def get(message:telebot.types.Message) -> None:
@@ -38,31 +53,34 @@ class Service(Singleton):
             /get all            return all telegram invitation links
             /get [unitCode]     return telegram invitation link for a specific unit
             """
-            replies = User(message.from_user.username, message.from_user.full_name, self._admins, self._db, self._logger).get(message)
+            replies = User(message.from_user.username, message.from_user.full_name, self._logger).get(message)
             for reply in replies:
-                await self._bot.reply_to(message, reply)
+                await self._telebot.reply_to(message, reply)
 
         @self._telebot.message_handler(commands=['admins'])
         async def adminlist( message:telebot.types.Message) -> None:
             """
             /admins             retrieve the list of administrators
             """
-            replies = User(message.from_user.username, message.from_user.full_name, self._admins, self._db, self._logger).adminlist(message)
+            replies = User(message.from_user.username, message.from_user.full_name, self._logger).adminlist(message)
             for reply in replies:
-                await self._bot.reply_to(message, reply)
+                await self._telebot.reply_to(message, reply)
         
         @self._telebot.message_handler(commands=["help"])
         async def help(message:telebot.types.Message) -> None:
             """
             /help               displays all available commands to the user
             """
+            replies = User(message.from_user.username, message.from_user.full_name, self._logger).help(message)
+            for reply in replies:
+                await self._telebot.reply_to(message, reply)
             try:
-                replies = Admin(message.from_user.username, message.from_user.full_name, self._admins, self._db, self._logger).help(message)
+                replies = Admin(message.from_user.username, message.from_user.full_name, self._logger).help(message)
             except NonAdminUserException:
-                replies = await User(message.from_user.username, message.from_user.full_name, self._admins, self._db, self._logger).help(message)
-            finally:
+                pass
+            else:
                 for reply in replies:
-                    await self._bot.reply_to(message, reply)
+                    await self._telebot.reply_to(message, reply)
 
         @self._telebot.message_handler(commands=["add"])
         async def add(message:telebot.types.Message) -> None:
@@ -70,13 +88,13 @@ class Service(Singleton):
             /add [unit code] ] [link] [title]       add a telegram group
             """
             try:
-                replies = Admin(message.from_user.username, message.from_user.full_name, self._admins, self._db, self._logger).add(message)
+                replies = Admin(message.from_user.username, message.from_user.full_name, self._logger).add(message)
             except NonAdminUserException:
                 self._logger.error(f"Non-admin user {message.from_user.username} attempted to use /add.")
                 await self._telebot.reply_to(message, "Fail. You are not authorised to perform /add.")
             else:
                 for reply in replies:
-                    await self._bot.reply_to(message, reply)
+                    await self._telebot.reply_to(message, reply)
 
         @self._telebot.message_handler(commands=["update"])
         async def update(message:telebot.types.Message) -> None:
@@ -85,13 +103,13 @@ class Service(Singleton):
             /update [unit code] name [new unit name]    update the name of the academic unit
             """
             try:
-                replies = Admin(message.from_user.username, message.from_user.full_name, self._admins, self._db, self._logger, self._telebot).update(message)
+                replies = Admin(message.from_user.username, message.from_user.full_name, self._logger).update(message)
             except NonAdminUserException:
                 self._logger.error(f"Non-admin user {message.from_user.username} attempted to use /update.")
                 await self._telebot.reply_to(message, "Fail. You are not authorised to perform /update.")
             else:
                 for reply in replies:
-                    await self._bot.reply_to(message, reply)
+                    await self._telebot.reply_to(message, reply)
 
         @self._telebot.message_handler(commands=["rm"])
         async def remove(message:telebot.types.Message) -> None:
@@ -99,11 +117,11 @@ class Service(Singleton):
             /rm [unitCode]      removes a telegram group for that unit code.
             """
             try:
-                replies = Admin(message.from_user.username, message.from_user.full_name, self._admins, self._db, self._logger).remove(message)
+                replies = Admin(message.from_user.username, message.from_user.full_name, self._logger).remove(message)
             except NonAdminUserException:
                 self._logger.error(f"Non-admin user {message.from_user.username} attempted to use /rm.")
                 await self._telebot.reply_to(message, "Fail. You are not authorised to perform /rm.")
             else:
                 for reply in replies:
-                    await self._bot.reply_to(message, reply)
+                    await self._telebot.reply_to(message, reply)
 
